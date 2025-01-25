10ad083f89ef748abd3d52cba3bdaa1fc87ac9c4
jiyunqq <243791377@qq.com>
2025-01-25 09:46:40 +0800

    GitHub Actions → GitHub Pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,15 +14,15 @@ const config = {
   favicon: 'img/reader_icon_24.png',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://jiyunqq.github.io/doc',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/doc',
+  baseUrl: '/doc/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'jiyunqq', // Usually your GitHub org/user name.
+  projectName: 'doc', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
